6d019fc9bbc873dab641751dd3b5211a627a9859
hossam ghieth <hossamghieth@gmail.com>
2024-03-19 04:34:41 +0200

    Added user basic crude functionality

---
+++ app/Http/Controllers/api/v1/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\api\v1\UserCollection;
+use App\Http\Resources\api\v1\UserResource;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new UserCollection(User::paginate());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        User::create($request->validated());
+        return response()->json([
+            'created' => true
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $user->update($request->validated());
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json('removed successfully', 200);
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('can add user', function () {
+    $this->withoutExceptionHandling();
+    $response = $this->postJson('/api/user', [
+        'name' => 'jon',
+        'email' => 'jon@email.com',
+        'password' => 'password',
+    ]);
+
+    $this->assertDatabaseCount('users', 1);
+    $response->assertStatus(201)->assertJson(['created' => true]);
+
+});
+
+
+it('can show users', function () {
+    $this->withoutExceptionHandling();
+    User::factory()->create();
+    $user = User::first();
+    $response = $this->get('/api/user');
+    $response->assertStatus(200);
+    $response->assertJsonCount(1, 'data');
+    $response->assertJsonPath('data.0.name', $user->name);
+});
+
+it('can allow user to his information', function () {
+    $this->withoutExceptionHandling();
+
+    User::factory()->create();
+    $user = User::first();
+
+    $response = $this->getJson('/api/user/' . $user->id);
+    $response->assertStatus(200);
+    $response->assertJsonPath('data.name', $user->name);
+    $response->assertJsonPath('data.email', $user->email);
+
+
+});
+
+it('can allow user to edit his information', function () {
+    $this->withoutExceptionHandling();
+    User::factory()->create();
+    $user = User::first();
+
+    $response = $this->patchJson('/api/user/' . $user->id, [
+        'name' => 'new jon -v-',
+        'password' => 'new password',
+    ]);
+
+    $newUser = User::all();
+    $response->assertStatus(200);
+    $this->assertEquals('new jon -v-', User::first()->name);
+    $response->assertJsonPath('data.name', User::first()->name);
+});
+
+it('can allow user to delete his account', function () {
+    $this->withoutExceptionHandling();
+
+    User::factory()->create();
+    $user = User::first();
+
+    $response = $this->deleteJson('/api/user/' . $user->id);
+
+    $response->assertStatus(200);
+    $this->assertDatabaseCount('users', 0);
+    $response->assertSee('removed successfully');
+});
