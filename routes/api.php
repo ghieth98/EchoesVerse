7b0aa233b0382f2d0b8dd40ff4cb425f47d9360f
hossam ghieth <hossamghieth@gmail.com>
2024-03-28 07:35:34 +0200

    Added comment route

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,15 @@ use App\Http\Controllers\api\v1\FollowController;
 use App\Http\Controllers\api\v1\PostController;
 use App\Http\Controllers\api\v1\ProfileController;
 use App\Http\Controllers\api\v1\UserController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\LikeController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('/user', UserController::class);
 Route::apiResource('/profile', ProfileController::class);
 Route::apiResource('/post', PostController::class);
+Route::apiResource('/post/{post}/comments/', CommentController::class);
+
 Route::post('/follow', [FollowController::class, 'store'])->name('follow.store');
 Route::delete('/unfollow', [FollowController::class, 'destroy'])->name('follow.destroy');
 Route::post('/like', [LikeController::class, 'store'])->name('like.store');
